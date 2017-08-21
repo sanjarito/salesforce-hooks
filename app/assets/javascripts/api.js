adb5f5ca2ce9d822394d06ed368ff71b201a4023
Sanjarito <santiago@nowhereprod.com>
2017-08-21 09:39:37 -0700

    Asda

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -1,14 +1,30 @@
 function submitForm(){
   alert("we are inside button function");
-
+  var myData;
      $.ajax({
-         url: 'https://santiago.pixfizz.com/v1/admin/users/2760630',
+         url: 'https://corporate.pixfizz.com/v1/admin/users.json',
          method: 'GET',
          dataType: 'jsonp',
          username: 'santiago@nowhereprod.com',
-         password: 'Detech28!!',
+         password: 'Detech28',
          success: function(data){
-           console.log('succes: '+data);
+          console.log('success')
+           myData = data
+            console.log(myData);
          }
        });
 }
+
+
+
+var auth = btoa('santiago@nowhereprod.com:Detech28');
+$.ajax({
+    type: 'GET',
+    url: 'https://corporate.pixfizz.com/v1/admin/users.json',
+    headers: {
+        "Authorization": "Basic " + auth
+    },
+    success : function(data) {
+    },
+
+});
