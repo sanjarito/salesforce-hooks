2f772685b9510c450b55443221db4a478aef9787
Sanjarito <santiago@nowhereprod.com>
2017-08-20 20:28:19 -0700

    all

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,17 +1,22 @@
 class WelcomeController < ApplicationController
 
-  # require 'rest_client'
-  # require 'json'
-  # require 'open-uri'
+  require 'rest_client'
+  require 'json'
+  require 'open-uri'
 
     # def index
-    #   USERNAME = "santiago@nowhereprod.com" # needed to access the APi
-    #     PASSWORD = "Habitat28" # needed to access the APi
-    #     API_BASE_URL = "https://corporate.pixfizz.com/v1/admin/users" # base url of the API
-    #
-    #
-    #     # user_api = RestClient::Resource.new('https://instantsignup.pixfizz.com', :user => USERNAME , :password => PASSWORD)
-    #     #      result = user_api["/v1/users/15961469.json"].put({:user =>{"first_name":"Floyd"}})
+      USERNAME = "santiago@nowhereprod.com" # needed to access the APi
+        PASSWORD = "Habitat28" # needed to access the APi
+        API_BASE_URL = "https://corporate.pixfizz.com/v1/admin/users" # base url of the API
+
+    uri = "#{API_BASE_URL}#.json"
+
+        # user_api = RestClient::Resource.new('https://instantsignup.pixfizz.com', :user => USERNAME , :password => PASSWORD)
+
+        RestClient::Request.execute method: :get, url: uri, user: USERNAME, password: PASSWORD
+        # rest_resource = RestClient::Resource.new(uri, USERNAME, PASSWORD)
+        # users = rest_resource.get
+
     # end
 
 end
